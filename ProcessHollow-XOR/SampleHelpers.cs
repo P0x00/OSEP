1b8a801cd3029c96f338291083677401234bc7fe
P0 <99042766+P0x00@users.noreply.github.com>
2024-03-25 10:13:51 +0000

    Process Hollow-XOR

---
+++ ProcessHollow-XOR/SampleHelpers.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static Process_Hollowing.Imports.Imports;
+
+internal static class SampleHelpers
+{
+    [DllImport("ntdll.dll")]
+    public static extern int ZwQueryInformationProcess(IntPtr hProcess, int procInformationClass, ref PROCESS_BASIC_INFORMATION procInformation, uint ProcInfoLen, ref uint retlen);
+}
